23f1e64466caf7fae1ab3cf842268476bcddc1bf
eto1302 <martinangelov1302@gmail.com>
2020-03-25 17:44:14 +0200

    This is the Forms and Validations Workshop

---
+++ workshop_forms_register/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <app-register>
+
+    </app-register>
+  </div>
+</template>
+
+<script>
+import AppRegister from './components/Register.vue'
+
+export default {
+  name: 'App',
+  components: {    
+    AppRegister
+  }
+}
+</script>
+
+
+<style>
+</style>
